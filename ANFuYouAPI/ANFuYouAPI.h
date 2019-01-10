56696cc4b178aa02b0fec2fd928700db35774a22
liudong <liudong.edward@gmail.com>
2019-01-10 18:57:37 +0800

    添加基础项目

---
+++ ANFuYouAPI/ANFuYouAPI.h
@@ -0,0 +1,37 @@
+//
+//  ANFuYouAPI.h
+//  ANFuYouAPI
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+//  Created by 刘栋 on 2019/1/10.
+//  Copyright © 2019 anotheren.com. All rights reserved.
+//
+
+#import <Foundtion/Foundtion.h>
+
+//! Project version number for ANFuYouAPI.
+FOUNDATION_EXPORT double ANFuYouAPIVersionNumber;
+
+//! Project version string for ANFuYouAPI.
+FOUNDATION_EXPORT const unsigned char ANFuYouAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ANFuYouAPI/PublicHeader.h>
+
+
